6a0a28520af4715bae7741441c5a8c5908215af0
Caleb San Lucas <cssan@puce.edu.ec>
2025-08-18 11:58:21 -0500

    Estilos Reportar Incidente

--- src/pages/ReportarIncidente.jsx
+++ src/pages/ReportarIncidente.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import "./ReportarIncidente.css";
 
 export const ReportarIncidente = () => {
   const [tipoIncidente, setTipoIncidente] = useState("accidente");
