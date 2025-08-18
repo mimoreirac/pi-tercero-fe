01dc37f27b485a4386c5333213f72cc522482ef8
mimoreirac <172557112+mimoreirac@users.noreply.github.com>
2025-08-17 22:50:39 -0500

    Agrega estilos para el perfil

--- src/pages/MiPerfil.jsx
+++ src/pages/MiPerfil.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
+import { Link } from "react-router-dom";
+import "./MiPerfil.css";
+import { MdArrowBack } from "react-icons/md";
 
 export const MiPerfil = () => {
   const { user } = useAuth();
@@ -54,43 +57,58 @@ export const MiPerfil = () => {
   }
 
   return (
-    <div>
-      <h2>Mi Perfil</h2>
-      <p>
-        <strong>Nombre:</strong> {user.dbUser.nombre}
-      </p>
-      <p>
-        <strong>Email:</strong> {user.firebaseUser.email}
-      </p>
-      <p>
-        <strong>Teléfono:</strong> {user.dbUser.numero_telefono}
-      </p>
+    <div className="perfil-container">
+      <div className="datos-usuario">
+        <h2>Mi Perfil</h2>
+        <p className="texto-datos-usuario">
+          <strong className="header-datos-usuario">Nombre:</strong>{" "}
+          {user.dbUser.nombre}
+        </p>
+        <p className="texto-datos-usuario">
+          <strong className="header-datos-usuario">Email:</strong>{" "}
+          {user.firebaseUser.email}
+        </p>
+        <p className="texto-datos-usuario">
+          <strong className="header-datos-usuario">Teléfono:</strong>{" "}
+          {user.dbUser.numero_telefono}
+        </p>
+      </div>
 
-      <h3>Mis Viajes</h3>
-      {viajes.length > 0 ? (
-        <ul>
-          {viajes.map((viaje) => (
-            <li key={viaje.id_viaje}>
-              {viaje.origen} - {viaje.destino} - {viaje.estado}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No has creado ningún viaje.</p>
-      )}
+      <div className="panel-viajes-incidentes">
+        <div className="viajes-usuario">
+          <h3>Mis Viajes</h3>
+          {viajes.length > 0 ? (
+            <ul>
+              {viajes.map((viaje) => (
+                <li key={viaje.id_viaje}>
+                  {viaje.origen} - {viaje.destino} - {viaje.estado}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No has creado ningún viaje.</p>
+          )}
+        </div>
 
-      <h3>Mis Incidentes</h3>
-      {incidentes.length > 0 ? (
-        <ul>
-          {incidentes.map((incidente) => (
-            <li key={incidente.id_incidente}>
-              {incidente.tipo_incidente}: {incidente.descripcion}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No has reportado ningún incidente.</p>
-      )}
+        <div className="incidentes-usuario">
+          <h3>Mis Incidentes</h3>
+          {incidentes.length > 0 ? (
+            <ul>
+              {incidentes.map((incidente) => (
+                <li key={incidente.id_incidente}>
+                  {incidente.tipo_incidente}: {incidente.descripcion}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No has reportado ningún incidente.</p>
+          )}
+        </div>
+      </div>
+      <Link to="/" className="boton-regreso">
+        <MdArrowBack />
+        Regresar al dashboard
+      </Link>
     </div>
   );
 };
